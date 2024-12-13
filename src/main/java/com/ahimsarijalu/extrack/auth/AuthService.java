9cb30ff0b40da43efd1f00b02429b19881defc3d
Rijalu Ahimsa <ahimsarijalu@gmail.com>
2024-12-13 10:08:12 +0700

    change register method in User Service to returning void instead of User

--- src/main/java/com/ahimsarijalu/extrack/auth/AuthService.java
+++ src/main/java/com/ahimsarijalu/extrack/auth/AuthService.java
@@ -2,7 +2,6 @@ package com.ahimsarijalu.extrack.auth;
 
 import com.ahimsarijalu.extrack.user.User;
 import com.ahimsarijalu.extrack.user.UserRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
